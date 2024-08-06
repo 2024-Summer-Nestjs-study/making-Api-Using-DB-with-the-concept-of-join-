c7a0065873b9d74ce0c2322fcffd72be59d2aed9
정재록 <jjlpower@naver.com>
2024-08-06 12:22:31 +0900

    Typeorm을 사용하여 DB에 연결

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { BoardEntity } from '../entity/board.entity';
+import { UserEntity } from '../entity/user.entity';
+import { UserController } from './user/user.controller';
+import { BoardController } from './board/board.controller';
+import { UserService } from './user/user.service';
+import { BoardService } from './board/board.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'power01',
+      database: 'jaerock',
+      entities: [BoardEntity, UserEntity], //전체
+      synchronize: true,
+    }),
+    BoardModule,
+    UserModule,
+  ],
+  controllers: [AppController, UserController, BoardController],
+  providers: [AppService, UserService, BoardService],
+  exports: [TypeOrmModule],
+})
+export class AppModule {}
