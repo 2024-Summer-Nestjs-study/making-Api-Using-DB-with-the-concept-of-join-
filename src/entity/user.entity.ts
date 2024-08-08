aea1c4638264b47a267c22ebd262bb240d61c646
정재록 <jjlpower@naver.com>
2024-08-09 05:42:51 +0900

    [추가] UserEntity에 @Unique 를 사용하여 id에 유니크값을 가지게 함

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, Unique } from 'typeorm';
 import { DefaultEntity } from './default.entity';
 
 @Entity()
+@Unique(['id'])
 export class UserEntity extends DefaultEntity {
   @Column()
   username: string;
