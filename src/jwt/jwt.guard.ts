3970d2eaed5e49ffd2d6eb9a7f6a9cef808e8f54
정재록 <jjlpower@naver.com>
2024-08-15 15:32:20 +0900

    [수정] accesstoken만 검토하도록 수정

--- src/jwt/jwt.guard.ts
+++ src/jwt/jwt.guard.ts
@@ -6,42 +6,30 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
-/**일단 여기는 깃허브에 있는거 복사한 것**/
 @Injectable()
 export class JwtGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const token = request.headers.authorization;
-    if (!token) {
+    const accessToken = request.headers.authorization;
+    if (!accessToken) {
       console.log('헤더 빔');
       throw new UnauthorizedException();
     }
     const secretA = 'qwer';
-    const secretR = 'asdf';
-    /**먼저 Acess토큰에 대한 검토 시작**/
+    /**Acess Token 검토**/
     try {
-      const payload = await this.jwtService.verifyAsync(token, {
+      const payload = await this.jwtService.verifyAsync(accessToken, {
         secret: secretA,
       });
-      request['user'] = payload;
+      request['user'] = payload.index;
     } catch (e) {
-      /**에러가 검출되면 secret에 대한 검토 시작**/
-      if (e.name === 'JsonWebTokenError') {
-        console.log('응 access아니야');
+      if (e.name === 'TokenExpiredError') {
+        throw new UnauthorizedException('access만료');
       }
-      try {
-        const payload = await this.jwtService.verifyAsync(token, {
-          secret: secretR,
-        });
-        request['user'] = payload;
-      } catch (e) {
-        if (e.name === 'JsonWebTokenError') {
-          throw new UnauthorizedException('Token이 맞아?');
-        }
-        console.log(e);
-        throw new UnauthorizedException();
+      if (e.name === 'JsonWebTokenError') {
+        throw new UnauthorizedException('Token이 맞아?');
       }
     }
     return true;
