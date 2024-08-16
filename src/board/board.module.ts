70472caed77a5aa5e877ac4d9674bb2ecf9a83a5
정재록 <jjlpower@naver.com>
2024-08-16 09:37:28 +0900

    [추가] 인증을 위한 jwtguard 추가

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -3,9 +3,10 @@ import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardEntity } from '../entity/board.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardEntity])],
+  imports: [TypeOrmModule.forFeature([BoardEntity]), JwtModule.register({})],
   controllers: [BoardController],
   providers: [BoardService],
   exports: [TypeOrmModule],
